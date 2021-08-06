9c180926b4710adf06e1aade312e06eb6ce55148
Ian <ianmat55@gmail.com>
2021-08-06 07:05:24 -0700

    added sql stuff

--- url_shortener.py
+++ url_shortener.py
@@ -1,8 +1,18 @@
 #takes url and shortens it to look nice. Using Flask, sqlite
-from flask import Flask, render_template
-import requests, sqlite3
+from flask import Flask, render_template, request, flash, redirect, url_for
+from hashids import Hashids
+import sqlite3
+
+def get_db_connection():
+	conn = sqlite3.connect('database.db')
+	conn.row_factory = sqlite3.Row
+	return conn
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this should be a secret random string'
+
+hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 
 @app.route('/')
 def index():
